c745fece5c698dd8f8e45a772cea24c7a965cef5
xchyyb <78303344+xchyyb@users.noreply.github.com>
2024-01-27 13:02:27 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '夏虫欲语冰', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个明年比今年大一岁的人', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '一个今年比去年大一岁的人', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://yhm97.tk', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, 随笔', // 网站关键词 英文逗号隔开
 
